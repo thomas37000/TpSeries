8ed5dde3eb6153998bd272b5acd4ec5720e2d137
Chalanson Thomas <thomas.chalanson@sfr.fr>
2022-06-08 09:31:47 +0200

    create 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { SerieComponent } from './components/serie/serie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+    SerieComponent
   ],
   imports: [
     BrowserModule,
